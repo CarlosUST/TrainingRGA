ca65fb12b1f58a7b9bab053fa1247169d91d3afb
CarlosUST <218964@ust.com>
2025-05-21 19:02:38 +0000

    L9Finished/CreatingaFormtoEditYourData

--- Day7/projectpilot/src/projects/ProjectsList.tsx
+++ Day7/projectpilot/src/projects/ProjectsList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectsListProps {
     projects: Project[];
@@ -11,6 +12,7 @@ function ProjectsList({projects}: ProjectsListProps) {
   const items = projects.map(project => (
     <div key={project.id} className='cols-sm'>
       <ProjectCard project={project}/>
+      <ProjectForm/>
     </div>
   ));
   return <div className='row'>{items}</div>
