65dffed3f63553974ef8fb7a58258e3cb4a85048
CarlosUST <218964@ust.com>
2025-05-27 17:14:27 +0000

    FormInNewProject

--- Day7/projectpilot/src/newProjects/NewProject.tsx
+++ Day7/projectpilot/src/newProjects/NewProject.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Project } from '../projects/Project';
+import ProjectForm from '../projects/ProjectForm';
 
 function NewProject() {
+  const handleSave = (project: Project) => {
+    console.log('Project Saved: ', project);
+  };
+
+  const handleCancel = () => {
+    console.log('Projection canceled');
+  };
+
+  const initialProject = new Project({
+    id: 0,
+    name: '',
+    description: '',
+    budget: 0,
+    isActive: false,
+  });
+
   return (
-    <h2>New Project</h2>
-  )
+    <div>
+      <h2>New Project</h2>
+    <   ProjectForm
+          project={initialProject}
+          onSave={handleSave}
+          onCancel={handleCancel}  
+    />
+    </div>
+  );
 }
 
 export default NewProject
